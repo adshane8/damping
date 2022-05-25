319dc89d781870dcdc43068e26ae180c1fb42855
Alexander Shane <alex@ccar-alsh7949-laptop.int.colorado.edu>
2022-05-25 12:06:54 -0600

    Swapped plus/minus signs in Jm_m1 and Jm_p1

--- src/integrand.cpp
+++ src/integrand.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace Eigen;
 
-void integrand::initialize(psd_model& f_in, double kperp_in, double kpar_in, 
+void integrand::initialize(psd_model& f_in, double kperp_in, double kpar_in,
                            double w_in, double n_in, int m_low_in, int m_hi_in, double wch_in,
                            double R_in, double L_in, double P_in, double S_in) {
 
@@ -14,7 +14,7 @@ void integrand::initialize(psd_model& f_in, double kperp_in, double kpar_in,
     w = w_in;
     m_low = m_low_in;   // Lowest resonance mode  (0 is landau, +- 1 is cyclotron)
     m_hi  = m_hi_in;    // Highest resonance mode
-    wch = wch_in;   
+    wch = wch_in;
 
     // Pointer to psd model:
     f = f_in;
@@ -23,7 +23,7 @@ void integrand::initialize(psd_model& f_in, double kperp_in, double kpar_in,
     R = R_in;
     L = L_in;
     P = P_in;
-    S = S_in;  
+    S = S_in;
 
 // % angle of wavenormal with respect to B0
     theta = atan2(kperp,kpar);
@@ -54,7 +54,7 @@ double Jm, Jm_m1, Jm_p1;
 double G1, G2;
 double ret;
 
-    
+
     // Evaluate for each resonance between m_low and m_hi:
     // Jm = jn(1, 2);
     // cout << "Jm: " << Jm << "\n";
@@ -62,8 +62,8 @@ double ret;
     for (int m = m_low; m <= m_hi; m++) {
         // evaluate Bessel functions
         Jm = jn(m, kperp*vperp/wch);
-        Jm_m1 = jn(m + 1, kperp*vperp/wch);
-        Jm_p1 = jn(m - 1, kperp*vperp/wch);
+        Jm_m1 = jn(m - 1, kperp*vperp/wch);
+        Jm_p1 = jn(m + 1, kperp*vperp/wch);
 
         vpar = (w - m*wch)/kpar;
 
@@ -72,7 +72,7 @@ double ret;
 
     // % Chen's derivation.  Kennel has accidentally swapped (R-n^2) and (L-n^2)!
     suminteg += (G1* ( (P-n_sq*st_sq)*(2*(L-n_sq)*vperp*Jm_p1*Jm_p1 + 2*vperp*(R-n_sq)*Jm_m1*Jm_m1 +
-                                       n_sq*st_sq*vperp*pow(Jm_p1-Jm_m1, 2)) 
+                                       n_sq*st_sq*vperp*pow(Jm_p1-Jm_m1, 2))
                        -n_sq*ct*st*(2*vpar*Jm*(Jm_p1*(R-n_sq)+Jm_m1*(L-n_sq)) +
                                     n_sq*ct*st*vperp*pow(Jm_p1-Jm_m1, 2)))
          +G2*(4*vpar*Jm*((L-n_sq)*(R-n_sq)+n_sq*st_sq*(S-n_sq))
@@ -102,13 +102,13 @@ double integrand::fG1(double vperp, double vpar) {
     double G1;
     // cout << "eps: " << eps << "\n";
     double d = DEL*abs(vperp);
-    
+
     if( d < 10*eps ) {
       d = 10*eps;
     }
 
     double df_dvperp = ( f.hybrid_psd(vperp+d, vpar) -
-                         f.hybrid_psd(vperp-d, vpar) ) 
+                         f.hybrid_psd(vperp-d, vpar) )
                         / (2*d);
 
     d = DEL*abs(vpar);
@@ -118,9 +118,9 @@ double integrand::fG1(double vperp, double vpar) {
     }
 
     double df_dvpar = ( f.hybrid_psd(vperp, vpar+d) -
-                        f.hybrid_psd(vperp, vpar-d) ) 
+                        f.hybrid_psd(vperp, vpar-d) )
                         / (2*d);
-    
+
     G1 = df_dvperp - (kpar/w)*(vpar*df_dvperp - vperp*df_dvpar);
     // cout << "df_perp: " << df_dvperp << " df_par: " << df_dvpar << " G1: " << G1 << "\n";
     return G1;
@@ -132,13 +132,13 @@ double integrand::fG2(double vperp, double vpar, double m) {
     double G1;
     // cout << "eps: " << eps << "\n";
     double d = DEL*abs(vperp);
-    
+
     if( d < 10*eps ) {
       d = 10*eps;
     }
 
     double df_dvperp = ( f.hybrid_psd(vperp+d, vpar) -
-                         f.hybrid_psd(vperp-d, vpar) ) 
+                         f.hybrid_psd(vperp-d, vpar) )
                         / (2*d);
 
     d = DEL*abs(vpar);
@@ -148,9 +148,9 @@ double integrand::fG2(double vperp, double vpar, double m) {
     }
 
     double df_dvpar = ( f.hybrid_psd(vperp, vpar+d) -
-                        f.hybrid_psd(vperp, vpar-d) ) 
+                        f.hybrid_psd(vperp, vpar-d) )
                         / (2*d);
-    
+
     double Jm = jn(m, kperp*vperp/wch);
 
     double G2 = Jm*(df_dvpar-(m*wch+eps)/(w*vperp+eps)*(vpar*df_dvperp - vperp*df_dvpar));
@@ -158,5 +158,3 @@ double integrand::fG2(double vperp, double vpar, double m) {
     return G2;
 
 }
-
-
